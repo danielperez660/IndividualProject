bc45e74b059f30fe4bbbd722e45fbae0e1aa3bf5
Daniel <danielperezcascon@gmail.com>
2019-10-30 11:40:58 +0000

    changes to the main backend of the app. Enabled use of bluetooth

--- app/src/main/java/sc17dpc/individualproject/MainMenu.java
+++ app/src/main/java/sc17dpc/individualproject/MainMenu.java
@@ -1,5 +1,9 @@
 package sc17dpc.individualproject;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -9,9 +13,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.widget.Toast;
+
 public class MainMenu extends AppCompatActivity {
 
     private TextView mTextMessage;
+    private BluetoothAdapter mBluetoothAdapter;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -20,7 +30,7 @@ public class MainMenu extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    mTextMessage.setText(R.string.clear);
                     return true;
                 case R.id.navigation_dashboard:
                     mTextMessage.setText(R.string.title_dashboard);
@@ -38,19 +48,85 @@ public class MainMenu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+
+        // Bluetooth check to see if it exists and if not it closes app
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+            Toast.makeText(this, R.string.ble_not_supported, Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
+        final BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        mBluetoothAdapter = bluetoothManager.getAdapter();
+
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(this, R.string.error_bluetooth_not_supported, Toast.LENGTH_SHORT).show();
+            finish();
+            return;
+        }
+
+        mTextMessage = findViewById(R.id.textView);
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        Button buttonTest = findViewById(R.id.registerButton);
+
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-
-        Button button = (Button) findViewById(R.id.registerButton);
-
-        button.setOnClickListener(new View.OnClickListener(){
+        buttonTest.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View v) {
-                
+                mTextMessage.append(String.valueOf(System.currentTimeMillis()) + "\n");
             }
         });
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Requests permission from the user to use bluetooth
+        if (!mBluetoothAdapter.isEnabled()) {
+            if (!mBluetoothAdapter.isEnabled()) {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, 1);
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // User chose not to enable Bluetooth.
+        if (requestCode == 1 && resultCode == Activity.RESULT_CANCELED) {
+            finish();
+            return;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+//    private void scanLeDevice(final boolean enable) {
+//        if (enable) {
+//            mBluetoothAdapter.stopLeScan(mLeScanCallback);
+//            mBluetoothAdapter.startLeScan(mLeScanCallback);
+//        } else {
+//            mBluetoothAdapter.stopLeScan(mLeScanCallback);
+//        }
+//    }
+//
+//    /**
+//     * Device scan callback.
+//     */
+//    private BluetoothAdapter.LeScanCallback mLeScanCallback = new BluetoothAdapter.LeScanCallback() {
+//        @Override
+//        public void onLeScan(final BluetoothDevice device, int rssi, byte[] scanRecord) {
+//            /**
+//             * Package data into Eddystone
+//             */
+//            final Eddystone eddystone = EddystoneClass.fromScanData(device, rssi, scanRecord);
+//            runOnUiThread(new Runnable() {
+//                @Override
+//                public void run() {
+//                    mLeDeviceListAdapter.addDevice(eddystone);
+//                    mLeDeviceListAdapter.notifyDataSetChanged();
+//                }
+//            });
+//        }
+//    };
 
 }
