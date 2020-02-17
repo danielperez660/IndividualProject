2ef759cb610ce5640db950d98c99e21a9bc36ecd
Daniel <danielperezcascon@gmail.com>
2020-02-17 13:42:38 +0000

    removed beacon persistance

--- app/src/main/java/sc17dpc/individualproject/BeaconControllerService.java
+++ app/src/main/java/sc17dpc/individualproject/BeaconControllerService.java
@@ -56,7 +56,8 @@ public class BeaconControllerService extends Service implements BeaconConsumer {
     public void onBeaconServiceConnect() {
         final Region region = new Region("DanielBeaconNew",null, null, null);
         Log.d("HomeMade", "Searching for beacons");
-
+        
+        beaconManager.setRegionStatePersistenceEnabled(false);
         beaconManager.addMonitorNotifier(new MonitorNotifier()  {
 
             // Finds a beacon and commences to track its distance
