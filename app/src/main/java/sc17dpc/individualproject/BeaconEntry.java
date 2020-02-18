e5bb05d633ea3eae6cbc7799531830c4bd83c408
Daniel <danielperezcascon@gmail.com>
2020-02-18 16:18:04 +0000

    Changes made to database collection and addition features

--- app/src/main/java/sc17dpc/individualproject/BeaconEntry.java
+++ app/src/main/java/sc17dpc/individualproject/BeaconEntry.java
@@ -4,4 +4,16 @@ public class BeaconEntry {
     public String beaconID;
     public String beaconName;
     public String position;
+
+    public String getBeaconID() {
+        return beaconID;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public String getBeaconName() {
+        return beaconName;
+    }
 }
--- app/src/main/java/sc17dpc/individualproject/DatabaseHelper.java
+++ app/src/main/java/sc17dpc/individualproject/DatabaseHelper.java
@@ -1,8 +1,12 @@
 package sc17dpc.individualproject;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -37,9 +41,9 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         String createBeaconTable = "CREATE TABLE " + TABLE_BEACONS +
-                                    "(" + KEY_BEACONS_BLUETOOTH_ID + "TEXT,"
-                                    + KEY_BEACONS_NAME + "TEXT,"
-                                    + KEY_BEACONS_POSITION + "TEXT" + ")";
+                "(" + KEY_BEACONS_BLUETOOTH_ID + "TEXT,"
+                + KEY_BEACONS_NAME + "TEXT,"
+                + KEY_BEACONS_POSITION + "TEXT" + ")";
         db.execSQL(createBeaconTable);
     }
 
@@ -52,18 +56,56 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         }
     }
 
-    public void addEntrie(){
+    public void addEntry(BeaconEntry beacon) {
+        ContentValues values = new ContentValues();
+        values.put(KEY_BEACONS_BLUETOOTH_ID, beacon.getBeaconID());
+        values.put(KEY_BEACONS_NAME, beacon.getBeaconName());
+        values.put(KEY_BEACONS_POSITION, beacon.getPosition());
 
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.insert(TABLE_BEACONS, null, values);
+        db.close();
     }
 
-    public List<BeaconEntry> getAllEntries(){
+    public List<BeaconEntry> getAllEntries() {
         List<BeaconEntry> beacons = new ArrayList<>();
+        String query = "SELECT * FROM " + TABLE_BEACONS;
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(query, null);
+
+        if(cursor.moveToFirst()){
+            while(!cursor.isAfterLast()) {
+                BeaconEntry newBeacon = new BeaconEntry();
+
+                newBeacon.beaconID = cursor.getString(cursor.getColumnIndex(KEY_BEACONS_BLUETOOTH_ID));
+                newBeacon.beaconName = cursor.getString(cursor.getColumnIndex(KEY_BEACONS_NAME));
+                newBeacon.position = cursor.getString(cursor.getColumnIndex(KEY_BEACONS_POSITION));
+
+                beacons.add(newBeacon);
+                cursor.moveToNext();
+            }
+        }
 
         return beacons;
     }
 
-    public BeaconEntry getEntry(String beaconID){
+    public BeaconEntry getEntry(String beaconID) {
         BeaconEntry beacon = new BeaconEntry();
+        String query = "SELECT * FROM " + TABLE_BEACONS + " WHERE " + KEY_BEACONS_BLUETOOTH_ID + " = " + "'" + beaconID + "'";
+
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.rawQuery(query, null);
+
+        if(cursor.moveToFirst()){
+            beacon.beaconID = cursor.getString(cursor.getColumnIndex(KEY_BEACONS_BLUETOOTH_ID));
+            beacon.beaconName = cursor.getString(cursor.getColumnIndex(KEY_BEACONS_NAME));
+            beacon.position = cursor.getString(cursor.getColumnIndex(KEY_BEACONS_POSITION));
+        }
+
+        Log.d("HomeMade", beacon.getBeaconID() + " " + beacon.getBeaconName());
+
         return beacon;
     }
 }
